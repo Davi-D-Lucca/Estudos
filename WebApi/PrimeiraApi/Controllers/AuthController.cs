5a47dd356157c7394533748f81ce038cd7e5f06b
Davi-D-Lucca <ddlucca10@gmail.com>
2026-09-23 19:58:58 -0300

    feat:Geracao de Token e Rota de Auth

---
+++ WebApi/PrimeiraApi/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApi.Model;
+using PrimeiraApi.Services;
+
+namespace PrimeiraApi.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth(string username, string password)
+        {
+            if(username == "admin" && password == "123")
+            {
+                var token = TokenService.GenerateToken(new Employee());
+            }
+
+            return BadRequest("Username or password invalid");
+        }
+    }
+}
