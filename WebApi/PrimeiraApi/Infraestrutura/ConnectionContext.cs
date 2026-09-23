b223b2575b49aa305f2bfd036acabd32108039d1
Davi-D-Lucca <ddlucca10@gmail.com>
2026-09-23 16:50:21 -0300

    feat:Criado rotas Post e Get - Name & Age

---
+++ WebApi/PrimeiraApi/Infraestrutura/ConnectionContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeiraApi.Model;
+
+namespace PrimeiraApi.Infraestrutura
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                "Server=localhost;" +
+                "Port=3305;Database=postgres;" +
+                "User Id=postgres;" +
+                "Password=password"
+            );
+
+    }
+}
---
+++ WebApi/PrimeiraApi/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeiraApi.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
